9d3583366c903d52545c3fbd59364ae1e09f8206
Mohamed Desoky <info@webbing-agency.com>
2025-05-07 04:58:54 +0300

    تحديث جديد في الكود

--- src/Components/dashboard/category/AddCategory.jsx
+++ src/Components/dashboard/category/AddCategory.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 // دالة API لإضافة كاتجوري
 const addCategory = async (formData) => {
   const response = await axios.post(
-    "http://194.164.77.238:8001/api/category",
+    "https://194.164.77.238:8001/api/category",
     formData,
     {
       headers: {
